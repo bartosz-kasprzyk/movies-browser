1f41ed6a01a90c256b0403ad4506c8c91eec9b92
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-02-06 19:06:40 +0100

    created styled component "Header"

---
+++ src/common/Header/styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+    background: ${({ theme }) => theme.color.woodsmoke};
+`
+
+export const Wrapper = styled.div`
+    width: 100%;
+    max-width: 1368px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 23px;
+    margin: 0 auto;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.tablet}) { 
+        flex-direction: column;
+        padding: 16px;
+    }
+`
