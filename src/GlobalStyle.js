aaae8a9c826118fdb7b6d544f1e918cb181bca4e
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-02-06 18:59:47 +0100

    removed font properties from the whole document and removed outline on focus

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -4,14 +4,15 @@ export const GlobalStyle = createGlobalStyle`
     html {
         box-sizing: border-box;
         font-family: 'Poppins', sans-serif;
-        font-weight: 600;
-        font-size: 36px;
     }
 
     *,
     ::after,
     ::before {
         box-sizing: inherit;
+    }
+    :focus {
+        outline: none;
     }
 
     body {
