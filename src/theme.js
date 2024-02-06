5d9a67c69e170c53eb8bcfb83c211c808c0b376c
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-02-06 19:00:10 +0100

    added new colors to the theme and changed media query breakpoints

--- src/theme.js
+++ src/theme.js
@@ -2,10 +2,12 @@ export const theme = {
     color: {
         white: "#FFFFFF",
         whisper: "#F5F5FA",
+        mystic: "#E4E6F0",
+        waterloo: "#7E839A",
         woodsmoke: "#18181B"
     },
     breakpoint: {
-        tablet: "767px",
-        phone: "480px",
+        tablet: "1278px",
+        phone: "767px",
     }
 };
