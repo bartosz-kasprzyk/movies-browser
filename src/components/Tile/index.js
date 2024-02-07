69ebb148bf59b9ce11941a8c3ebcb90b70e618cb
Przemysław Pacek <ppacek@op.pl>
2024-02-07 20:35:19 +0100

    Row style added

--- src/components/Tile/index.js
+++ src/components/Tile/index.js
@@ -9,10 +9,18 @@ export const Tile = styled.article`
     gap: 40px;
 `;
 
+export const Row = styled.ul`
+    list-style: none;
+    display: flex;
+    flex-basis: 16.66%;
+    padding-left: 0;
+    gap: 24px;
+`;
+
 export const PersonTile = styled.li`
     width: 208px;
     height: 339px;
     box-shadow: 0 4px 12px 0 rgba(186, 199, 213, 0.5);
     border-radius: 5px;
-    padding: 16px;
+    margin-top: 8px;
 `;
