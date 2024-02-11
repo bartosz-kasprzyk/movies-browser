0b33db4d130c8376b568eb990ad718a296f06a0f
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-02-11 16:48:19 +0100

    added "Pagination" to the App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
 import { GlobalStyle } from './GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
+import { Pagination } from './common/Pagination';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       projekt
+      <Pagination />
     </ThemeProvider>
   );
 }
