6150e99da710561a5baf98a478dc4de26e02a957
Vitalii Tomenko <vitalikxxx985@gmail.com>
2024-02-11 14:19:42 +0100

    Added styled to NoResultsText

--- src/common/NoResults/styled.js
+++ src/common/NoResults/styled.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {ReactComponent as Picture} from "../../images/Picture.svg"
+import { ReactComponent as Picture } from "../../images/Picture.svg"
 
 export const StyledHeader = styled.div`
     margin: 56px;
@@ -11,4 +11,11 @@ export const StyledImages = styled(Picture)`
     position: relative;
     width: 100%;
     height: 100%;
+`;
+
+export const NoResultsText = styled.p`
+text-align: center;
+margin-top: 20px;
+font-size: 18px;
+color: black;
 `;
