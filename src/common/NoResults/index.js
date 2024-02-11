daf3bf36d5237fd3fbef302ec64875c7cac5bb81
Vitalii Tomenko <vitalikxxx985@gmail.com>
2024-02-11 14:20:13 +0100

    Added NoResultsText

--- src/common/NoResults/index.js
+++ src/common/NoResults/index.js
@@ -1,11 +1,12 @@
 import { GlobalStyle } from "../../GlobalStyle";
 import Header from "../../common/Header";
-import { StyledHeader, StyledImages } from "./styled";
+import { NoResultsText, StyledHeader, StyledImages } from "./styled";
 
-const NoResults = () => {
+const NoResults = ({ query }) => {
     <GlobalStyle>
         <StyledHeader>
             <Header />
+            <NoResultsText>Sorry, there are no results for "{query}"</NoResultsText>
             <StyledImages />
         </StyledHeader>
     </GlobalStyle>
