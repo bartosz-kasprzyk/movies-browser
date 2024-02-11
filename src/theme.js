820d16fd889dba107cf4d577e0de0c69ae85534c
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-02-11 16:47:10 +0100

    added colors to the theme

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,16 @@
 export const theme = {
     color: {
-        white: "#FFFFFF"
+        white: "#FFFFFF",
+        whisper: "#F5F5FA",
+        mystic: "#E4E6F0",
+        waterloo: "#7E839A",
+        woodsmoke: "#18181B",
+        mineShaft: "#333333",
+        pattensBlue: "#D6E4FF",
+        scienceBlue: "#0044CC"
     },
     breakpoint: {
-        tablet: "767px",
-        phone: "480px",
+        tablet: "1278px",
+        phone: "767px",
     }
 };
