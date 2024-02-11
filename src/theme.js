b06d80e33e07266fb4ad6c2fdf38b660ff49bb5e
Przemysław Pacek <ppacek@op.pl>
2024-02-11 21:42:33 +0100

    changed pone width

--- src/theme.js
+++ src/theme.js
@@ -3,12 +3,13 @@ export const theme = {
         white: "#FFFFFF",
         whisper: "#F5F5FA",
         mystic: "#E4E6F0",
+        waterloo: "#7E839A",
+        woodsmoke: "#18181B",
         stormGrey: "#74788B",
         heater: "#BAC7D5",
         black: "#000000",
     },
     breakpoint: {
-        tablet: "767px",
-        phone: "480px",
+        phone: "767px",
     }
 };
