be312f0c1862bb436a4fe73a1082bb4e05107bdb
Przemysław Pacek <ppacek@op.pl>
2024-02-14 17:08:00 +0100

    changed 'div' into 'section'

--- src/features/MoviePage/Content/Crew/styled.js
+++ src/features/MoviePage/Content/Crew/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Content = styled.div`
+export const Content = styled.section`
     margin-top: 64px;
 
     @media(max-width: ${({ theme }) => theme.breakpoint.phone}) {
