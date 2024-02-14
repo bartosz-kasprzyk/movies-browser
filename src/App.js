1bee285b9ba22c7ac4f603892208106659ab84e6
Vitalii Tomenko <vitalikxxx985@gmail.com>
2024-02-14 21:42:58 +0100

    Added Loading to the App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Pagination } from './common/Pagination';
 import { Container } from './common/Container';
 import { Header } from './common/Header';
 import { HashRouter } from 'react-router-dom';
+import Loading from './common/Loading/script';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <HashRouter>
         <Header />
         <Container>
-          projekt
+         <Loading />
         </Container>
         <Pagination />
       </HashRouter>
