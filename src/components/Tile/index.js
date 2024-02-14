5b0dee24dbb07ae4c8508e977f376d53e5bd8645
Przemysław Pacek <ppacek@op.pl>
2024-02-14 17:07:35 +0100

    changed 'li' into 'ul'

--- src/components/Tile/index.js
+++ src/components/Tile/index.js
@@ -30,7 +30,9 @@ export const Row = styled.ul`
     }
 `;
 
-export const PersonTile = styled.li`
+export const PersonTile = styled.ul`
+    list-style: none;
+    padding-left: 0;
     box-shadow: 0 4px 12px 0 rgba(186, 199, 213, 0.5);
     border-radius: 5px;
     margin-top: 8px;
