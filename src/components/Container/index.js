781faf3dd70fcfebe6c71b0434daec2871d7775c
Przemysław Pacek <ppacek@op.pl>
2024-02-15 17:06:08 +0100

    copied container from main branch

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,6 +1,15 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    width: 90%;
     max-width: 1368px;
-    margin: auto;
-`;
+    margin: 56px auto;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.tablet}) { 
+        margin: 40px auto;
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.phone}) { 
+        margin: 24px auto;
+    }
+`
