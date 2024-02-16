082202f2f3cf47c05f21aefc28426032b595d43a
Vitalii Tomenko <vitalikxxx985@gmail.com>
2024-02-16 22:00:49 +0100

    added style for phone and chang margin to the text

--- src/common/NoResults/styled.js
+++ src/common/NoResults/styled.js
@@ -6,14 +6,24 @@ export const StyledHeader = styled.div`
 `;
 
 export const StyledImages = styled(Picture)`
-    width: 668px;
-    height: 508px;
-    margin-left: 320px;
+    width: 90%;
+    max-width: 668px;
+    height: auto;
+    margin: 0 auto;
+
+    @media (min-width: 768px) {
+        margin: 0 0 20px 300px;
+    }
+
 `;
 
 export const NoResultsText = styled.p`
 text-align: left;
-margin: 56px;
+margin: 40px;
 font-size: 36px;
 font-weight: 600;
+
+@media (max-width: ${({ theme }) => theme.breakpoint.phone}) {
+    margin: auto;
+}
 `;
