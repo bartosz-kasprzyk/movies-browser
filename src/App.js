073dcae6439f6b68a823a96baea510d9568b235c
Przemysław Pacek <ppacek@op.pl>
2024-02-18 00:08:25 +0100

    changed App.js and Index.js according to main branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
+import { GlobalStyle } from './GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
 import { Container } from './common/Container';
+import { Header } from './common/Header';
+import { HashRouter } from 'react-router-dom';
 import MoviePage from './features/MoviePage';
 
 function App() {
   return (
-    <Container>
-      <MoviePage />
-    </Container>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <HashRouter>
+        <Header />
+        <Container>
+          <MoviePage />
+        </Container>
+      </HashRouter>
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from './GlobalStyle';
-import { theme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
       <App />
-    </ThemeProvider>
   </React.StrictMode>
 );
 
