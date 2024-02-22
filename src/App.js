f1fc17538729c267044cd9b830891ab81b10159b
Przemysław Pacek <ppacek@op.pl>
2024-02-22 16:13:29 +0100

    removed ThemeProvider and GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import { GlobalStyle } from './GlobalStyle';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './theme';
 import { Container } from './common/Container';
 import { Header } from './common/Header';
 import { HashRouter } from 'react-router-dom';
@@ -8,15 +5,12 @@ import MovieList from './features/MovieList';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
       <HashRouter>
         <Header />
         <Container>
           <MovieList />
         </Container>
       </HashRouter>
-    </ThemeProvider>
   );
 }
 
