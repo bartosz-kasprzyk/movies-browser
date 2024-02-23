d63597e5037cd0ddc7975bdd8c2848cded2c163a
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-02-23 19:47:05 +0100

    added a color and a few new resolutions to theme.js

--- src/theme.js
+++ src/theme.js
@@ -4,6 +4,7 @@ export const theme = {
         whisper: "#F5F5FA",
         mystic: "#E4E6F0",
         waterloo: "#7E839A",
+        stormGrey: "#74788B",
         woodsmoke: "#18181B",
         mineShaft: "#333333",
         pattensBlue: "#D6E4FF",
@@ -11,7 +12,10 @@ export const theme = {
         black: "#000000"
     },
     breakpoint: {
+        laptop: "1480px",
         tablet: "1278px",
+        tabletPortrait: "990px",
         phone: "767px",
+        phonePortrait: "481px"
     }
 };
