0c680d9aa8d346b089af331769e9f7095155fe64
Vitalii Tomenko <vitalikxxx985@gmail.com>
2024-02-24 14:56:35 +0100

    create API.js

---
+++ src/common/API/API.js
@@ -0,0 +1,6 @@
+export const URL = " https://api.themoviedb.org/3";
+export const key = "62fc1ce4679a666d8338e7511e805e38";
+
+export const language = `${URL}/tv/1399?language=en-US`;
+export const searchAPI = `${URL}/search/movie?include_adult=false&language=en-US&page=1`
+
