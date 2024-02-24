bb663d7bbd71550f575703dc9e780d87b8fe0170
Przemysław Pacek <ppacek@op.pl>
2024-02-24 23:47:06 +0100

    changed flex to grid

--- src/features/MovieList/styled.js
+++ src/features/MovieList/styled.js
@@ -21,15 +21,28 @@ export const MainTitle = styled.h1`
 `;
 
 export const Content = styled.span`
-    display: flex;
-    flex-wrap: wrap;
+    display: grid;
+    grid-template-columns: repeat(4, auto);
     justify-content: center;
     gap: 24px;
     margin: 0 auto;
     margin-bottom: 40px;
     padding-left: 0;
 
+    @media(max-width: ${({ theme }) => theme.breakpoint.notebook}) {
+        grid-template-columns: repeat(3, auto);
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.tabletTwo}) {
+        grid-template-columns: repeat(2, auto);
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.phoneTwo}) {
+        grid-template-columns: repeat(1, auto);
+    }
+
     @media(max-width: ${({ theme }) => theme.breakpoint.phonePortrait}) {
+        grid-template-columns: repeat(1, auto);
         width: 320px;
         gap: 16px;
         margin-bottom: 32px;
