8ca8310fe30af4bf03641ac00e9fffdf31540876
Vitalii Tomenko <vitalikxxx985@gmail.com>
2024-02-24 14:58:47 +0100

    create SearchResults

---
+++ src/common/SearchResults/index.js
@@ -0,0 +1,28 @@
+import NoResults from "../NoResults";
+import Error from "../Error";
+import Loading from "../Loading/script";
+import { PersonList } from "../../features/PersonList";
+import {useSearchResults} from "./useSearchResults";
+
+const SearchResults = () => {
+    const { searchResults, loading, error } = useSearchResults();
+    if (loading) {
+        return <Loading />;
+    }
+
+    if (error) {
+        return <Error />;
+    }
+
+    if (searchResults.lenght === 0) {
+        return <NoResults />;
+    }
+
+    return (
+        <>
+            <PersonList />
+        </>
+    );
+};
+
+export default SearchResults;
