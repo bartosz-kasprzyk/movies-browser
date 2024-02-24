e1f37ed781fc42e0d52e5fb72e7b38c0ebab4895
Vitalii Tomenko <vitalikxxx985@gmail.com>
2024-02-24 14:59:12 +0100

    create useSearchResults

---
+++ src/common/SearchResults/useSearchResults.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { fetchAPI } from "../API/fetchAPI";
+import { key, language, URL } from "../API/API";
+
+export const useSearchResults = () => {
+    const [searchResults, setSearchResults] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const location = useLocation();
+    const page = useSelector(state => state.pagination.page);
+    const query = (new URLSearchParams(location.search)).get("query");
+    const moviesPage = location.pathname.includes("/movies");
+    const movieOrPerson = moviesPage ? "movie" : "person";
+    const fetchingURL = `${URL}/search/${movieOrPerson}?api_key=${key}&query=${query}&page=${page}&language=${language}`;
+
+    useEffect(() => {
+        const getSearchResults = async () => {
+            try {
+                setLoading(true);
+                const response = await fetchAPI(fetchingURL);
+                setSearchResults(response);
+            } catch (error) {
+                setError("Error while fetching data from external API", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        getSearchResults();
+
+    }, [query, page, moviesPage]);
+
+    return { searchResults, loading, error };
+};
