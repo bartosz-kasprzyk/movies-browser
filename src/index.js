1d664bb735de0e6d0a871c5509221407ffae0c7f
Przemysław Pacek <ppacek@op.pl>
2024-02-25 18:10:11 +0100

    moved theme and GlobalStyle to index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
+import { GlobalStyle } from './GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App />
-  </React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+  </React.StrictMode >
 );
 
 // If you want to start measuring performance in your app, pass a function
