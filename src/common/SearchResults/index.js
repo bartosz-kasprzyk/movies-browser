bfe22215df27cdce4a75d604783ba2c6346aceba
Vitalii Tomenko <vitalikxxx985@gmail.com>
2024-02-25 15:09:47 +0100

    a litle changes

--- src/common/SearchResults/index.js
+++ src/common/SearchResults/index.js
@@ -2,7 +2,7 @@ import NoResults from "../NoResults";
 import Error from "../Error";
 import Loading from "../Loading/script";
 import { PersonList } from "../../features/PersonList";
-import {useSearchResults} from "./useSearchResults";
+import { useSearchResults } from "./useSearchResults";
 
 const SearchResults = () => {
     const { searchResults, loading, error } = useSearchResults();
