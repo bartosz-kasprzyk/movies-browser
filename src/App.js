924a3ddeae470e9572aceba952b9df3d61c98978
Przemysław Pacek <ppacek@op.pl>
2024-02-26 21:27:59 +0100

    added components links to App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,31 @@ import { Container } from './common/Container';
 import { Header } from './common/Header';
 import { HashRouter } from 'react-router-dom';
 import { PersonPage } from './features/PersonPage';
+import MovieList from './features/MovieList';
 
 function App() {
   return (
-    <HashRouter>
+    <HashRouter basename="/movies-browser">
       <Header />
+
       <Container>
-        <PersonPage />
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/movies" />
+          </Route>
+          <Route path="/movie">
+            <MoviePage />
+          </Route>
+          <Route path="/people">
+            <PersonList />
+          </Route>
+          <Route path="/person">
+            <PersonPage />
+          </Route>
+        </Switch>
       </Container>
     </HashRouter>
-  );
-}
+  )
+};
 
 export default App;
