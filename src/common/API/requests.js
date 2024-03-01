55460e696a286526adec9045005a648aaba8b82b
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-03-01 16:03:53 +0100

    added all api requests

---
+++ src/common/API/requests.js
@@ -0,0 +1,20 @@
+export const movie_details = "https://api.themoviedb.org/3/movie/{movie_id}?api_key=f34f6027eb3f2b88611be3cc5793a551&language=en-US"
+
+export const movie_credits = "https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=f34f6027eb3f2b88611be3cc5793a551&language=en-US"
+
+export const movie_popular = "https://api.themoviedb.org/3/movie/popular?api_key=f34f6027eb3f2b88611be3cc5793a551&language=en-US"
+
+
+export const person_details = "https://api.themoviedb.org/3/person/{person_id}?api_key=f34f6027eb3f2b88611be3cc5793a551&language=en-US"
+
+export const person_credits = "https://api.themoviedb.org/3/person/{person_id}/movie_credits?api_key=f34f6027eb3f2b88611be3cc5793a551&language=en-US"
+
+export const person_popular = "https://api.themoviedb.org/3/person/popular?api_key=f34f6027eb3f2b88611be3cc5793a551&language=en-US"
+
+
+export const search_movie = "https://api.themoviedb.org/3/search/movie?api_key=f34f6027eb3f2b88611be3cc5793a551&language=en-US"
+
+export const search_person = "https://api.themoviedb.org/3/search/person?api_key=f34f6027eb3f2b88611be3cc5793a551&language=en-US"
+
+
+export const genres_movielist = "https://api.themoviedb.org/3/genre/movie/list?api_key=f34f6027eb3f2b88611be3cc5793a551&language=en-US"
