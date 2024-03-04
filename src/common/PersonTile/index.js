250c9f380cab9d48f07a6a8542651f8fc8c67c6f
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-03-04 16:57:46 +0100

    added no_poster image and two image sizes for PersonTile

--- src/common/PersonTile/index.js
+++ src/common/PersonTile/index.js
@@ -1,13 +1,22 @@
+import { useScreenWidth } from '../../useScreenWidth';
 import { Description, Grid, StyledImage, StyledSubtitle, StyledTitle, Wrapper } from './styled';
+import no_poster from "../../images/no poster.png"
 
-export const PersonTile = ({ image, title, subtitle }) => (
-    <Wrapper>
-        <Grid>
-            <StyledImage src={image} />
-            <Description>
-                <StyledTitle>{title}</StyledTitle>
-                <StyledSubtitle>{subtitle}</StyledSubtitle>
-            </Description>
-        </Grid>
-    </Wrapper>
-);
+export const PersonTile = ({ image, title, subtitle }) => {
+    const screenWidth = useScreenWidth();
+
+    return (
+        <Wrapper>
+            <Grid>
+                {image
+                    ? <StyledImage src={"https://image.tmdb.org/t/p/" + (screenWidth > 767 ? "w342" : "w154") + image} />
+                    : <StyledImage src={no_poster} />
+                }
+                <Description>
+                    <StyledTitle>{title}</StyledTitle>
+                    <StyledSubtitle>{subtitle}</StyledSubtitle>
+                </Description>
+            </Grid>
+        </Wrapper>
+    )
+};
