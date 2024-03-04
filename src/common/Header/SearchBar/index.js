c3f488ff8237112ef302e672d8b0359c5c0e7e44
Vitalii Tomenko <vitalikxxx985@gmail.com>
2024-03-04 17:59:23 +0100

    change SearchBar

--- src/common/Header/SearchBar/index.js
+++ src/common/Header/SearchBar/index.js
@@ -1,10 +1,25 @@
 import { Wrapper, SearchIcon, SearchInput } from "./styled";
+import { useState } from "react";
 
-export const SearchBar = () => (
-    <Wrapper>
-        <SearchIcon />
-        <SearchInput
-            placeholder="Search for movies..."
-        />
-    </Wrapper>
-);
+export const SearchBar = ({ onSearch }) => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearch = (e) => {
+        setSearchTerm(e.target.value);
+        if (e.key === "Enter") {
+            onSearch(searchTerm);
+        }
+    };
+
+    return (
+        <Wrapper>
+            <SearchIcon />
+            <SearchInput
+                placeholder="Search for movies..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                onKeyDown={handleSearch}
+            />
+        </Wrapper>
+    );
+}
