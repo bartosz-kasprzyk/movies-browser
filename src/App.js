d907c657da718518b23ea6b37e545ba6238fb74c
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-03-06 18:14:34 +0100

    added every possible subpage into App.js for review purposes only

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import { Header } from './common/Header';
 import { HashRouter } from 'react-router-dom';
 import { Container } from './common/Container';
 import { PersonPage } from './features/PersonPage';
+import { PersonList } from './features/PersonList';
+import MovieList from './features/MovieList';
+import MoviePage from './features/MoviePage';
 
 function App() {
   return (
     <HashRouter>
+      <Header />
+      <MoviePage />
+
+      <Header />
+      <Container>
+        <MovieList />
+      </Container>
+
+      <Header />
+      <Container>
+        <PersonList />
+      </Container>
+
       <Header />
       <Container>
         <PersonPage />
