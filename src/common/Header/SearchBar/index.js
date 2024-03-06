00f985c80545e78579196437a58e29031ed70e19
Przemysław Pacek <ppacek@op.pl>
2024-03-06 15:54:36 +0100

    added "Search for people..." to SearchBar

--- src/common/Header/SearchBar/index.js
+++ src/common/Header/SearchBar/index.js
@@ -1,10 +1,18 @@
 import { Wrapper, SearchIcon, SearchInput } from "./styled";
+import { useLocation } from "react-router-dom/cjs/react-router-dom";
+import { toMovie, toMovies } from "../../../routes";
 
-export const SearchBar = () => (
-    <Wrapper>
-        <SearchIcon />
-        <SearchInput
-            placeholder="Search for movies..."
-        />
-    </Wrapper>
-);
+export const SearchBar = () => {
+    const location = useLocation();
+    const isMoviesPage = location.pathname === toMovies();
+    const isMoviePage = location.pathname === toMovie();
+
+    return (
+        <Wrapper>
+            <SearchIcon />
+            <SearchInput
+                placeholder={isMoviesPage || isMoviePage ? "Search for movies..." : "Search for people..."}
+            />
+        </Wrapper>
+    );
+};
