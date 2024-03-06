d237fa29044c7381813f2bb88c61a155d78d3dd7
Przemysław Pacek <ppacek@op.pl>
2024-03-06 22:21:20 +0100

    removed homepage from routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 export const toMovie = ({ id } = { id: ":id" }) => `/movie/${id}`;
 export const toPerson = ({ id } = { id: ":id" }) => `/person/${id}`;
 
-export const toHomepage = () => "/homepage";
 export const toMovies = () => "/movies";
 export const toPeople = () => "/people";
