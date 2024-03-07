247be063a1a3da1392764a2634eb0d99f23abcc0
Przemysław Pacek <ppacek@op.pl>
2024-03-07 20:46:26 +0100

    changed for 'movies' and 'people'

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
-export const toMovie = ({ id } = { id: ":id" }) => `/movie/${id}`;
-export const toPerson = ({ id } = { id: ":id" }) => `/person/${id}`;
+export const toMovie = ({ id } = { id: ":id" }) => `/movies/${id}`;
+export const toPerson = ({ id } = { id: ":id" }) => `/people/${id}`;
 
 export const toMovies = () => "/movies";
 export const toPeople = () => "/people";
