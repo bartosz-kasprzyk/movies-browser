cb9eae754c8067846832129d1d2581903ccf79fe
Przemysław Pacek <ppacek@op.pl>
2024-03-10 01:37:20 +0100

    added working pagination

--- src/features/MovieList/usePopularMovies.js
+++ src/features/MovieList/usePopularMovies.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import { url_back, url_front_movie } from "../../movies/api";
 
 export const usePopularMovies = () => {
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const currentPage = parseInt(searchParams.get("page")) || 1;
+
+    const [totalPagesMovies, setTotalPagesMovies] = useState(1);
 
     const url = `${url_front_movie}popular${url_back}`;
 
@@ -12,9 +18,25 @@ export const usePopularMovies = () => {
     });
 
     useEffect(() => {
+        const gettotalPagesMovies = async () => {
+            try {
+                for (let i = 500; i <= 1000000; i++) {
+                    const response = await axios.get(`${url}&page=${i}`);
+
+                    if (response.data.results && response.data.results.length > 0) {
+                        setTotalPagesMovies(i);
+                    }
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        gettotalPagesMovies();
+
         const getPopularMovies = async () => {
             try {
-                const response = await axios.get(url);
+                const response = await axios.get(`${url}&page=${currentPage}`);
 
                 setPopularMovies({
                     status: "success",
@@ -29,7 +51,7 @@ export const usePopularMovies = () => {
         };
 
         setTimeout(getPopularMovies, 300);
-    }, []);
+    }, [url, currentPage]);
 
-    return { popularMovies };
+    return { popularMovies, totalPagesMovies };
 };
--- src/features/PersonList/usePopularPeople.js
+++ src/features/PersonList/usePopularPeople.js
@@ -1,20 +1,42 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import { url_back, url_front_person } from "../../movies/api";
 
-const page = "1";
-const url = `${url_front_person}popular${url_back}&page=${page}`;
-
 export const usePopularPeople = () => {
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const currentPage = parseInt(searchParams.get("page")) || 1;
+
+    const [totalPagesPeople, setTotalPagesPeople] = useState(1);
+
+    const url = `${url_front_person}popular${url_back}`;
+
     const [popularPeople, setPopularPeople] = useState({
         status: "loading",
-        data: [],
     });
 
     useEffect(() => {
+        const getTotalPages = async () => {
+            try {
+                for (let i = 500; i <= 1000000; i++) {
+                    const response = await axios.get(`${url}&page=${i}`);
+
+                    if (response.data.results && response.data.results.length > 0) {
+                        setTotalPagesPeople(i);
+                    }
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        getTotalPages();
+
         const getPopularPeople = async () => {
             try {
-                const response = await axios.get(url);
+                const response = await axios.get(`${url}&page=${currentPage}`);
+
                 setPopularPeople({
                     status: "success",
                     data: response.data,
@@ -28,7 +50,7 @@ export const usePopularPeople = () => {
         };
 
         setTimeout(getPopularPeople, 300);
-    }, []);
+    }, [url, currentPage]);
 
-    return { popularPeople };
+    return { popularPeople, totalPagesPeople };
 };
