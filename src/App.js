21a22ac08b7600b1cb87ae40a0815656d640f804
Przemysław Pacek <ppacek@op.pl>
2024-03-11 16:04:06 +0100

    added scroll up

--- src/App.js
+++ src/App.js
@@ -6,11 +6,29 @@ import { PersonPage } from './features/PersonPage';
 import { PersonList } from './features/PersonList';
 import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { toMovie, toMovies, toPeople, toPerson } from './routes';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+
+function ScrollToTop() {
+  const history = useHistory();
+
+  useEffect(() => {
+    const unlisten = history.listen(() => {
+      window.scrollTo(0, 0);
+    });
+    return () => {
+      unlisten();
+    };
+  }, [history]);
+
+  return null;
+}
 
 function App() {
   return (
     <HashRouter basename="/movies-browser">
       <Header />
+      <ScrollToTop />
 
       <Switch>
         <Route path={toMovie()}>
