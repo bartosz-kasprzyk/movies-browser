44bf65984195cb4604106d0e9558c43a0cb7d1d6
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-03-14 19:05:08 +0100

    added two new routes to the App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Header } from './common/Header';
 import MovieList from './features/MovieList';
 import MoviePage from './features/MoviePage';
+import SearchResults from './features/SearchResults';
 import { PersonPage } from './features/PersonPage';
 import { PersonList } from './features/PersonList';
 import { HashRouter, Route } from 'react-router-dom';
@@ -12,6 +13,14 @@ function App() {
     <HashRouter>
       <Header />
       <Switch>
+        <Route path="/movies/search">
+          <SearchResults />
+        </Route>
+
+        <Route path="/people/search">
+          <SearchResults />
+        </Route>
+
         <Route path={toMovie()}>
           <MoviePage />
         </Route>
