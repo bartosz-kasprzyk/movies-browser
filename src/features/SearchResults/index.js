2820f956b64c760cb5de03c64bef197091de9396
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-03-14 18:59:23 +0100

    temporary SearchResults component

---
+++ src/features/SearchResults/index.js
@@ -0,0 +1,88 @@
+import { Content, Grid, MainTitle } from './styled';
+import { Pagination } from '../../common/Pagination';
+import { Container } from '../../common/Container';
+import { useQueryParameter } from '../../common/Header/SearchBar/queryParameters';
+import { useSearchResults } from './useSearchResults';
+import { useGenres } from '../../useGenres';
+import Loading from '../../common/Loading';
+import Error from '../../common/Error';
+import { MovieTile } from '../../common/MovieTile';
+import NoResults from '../../common/NoResults';
+import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+import { PersonTile } from '../../common/PersonTile';
+
+const SearchResults = () => {
+    const query = useQueryParameter("query");
+
+    const location = useLocation();
+    const moviePage = location.pathname.includes("/movies");
+
+    const { searchResults } = useSearchResults();
+    const search_quantity = searchResults.data.total_results;
+    const search_list = searchResults.data.results;
+    const search_list_sorted = search_list?.sort((a, b) => b.popularity - a.popularity);
+
+    const { genres } = useGenres();
+    const genre_list = genres.data;
+
+    return (
+        <>
+            {searchResults.status === "loading" ? (
+                <Container>
+                    <MainTitle>Search results for "{query}"</MainTitle>
+                    <Loading />
+                </Container>
+            )
+                : searchResults.status === "error" ? (
+                    <Error />
+                )
+                    : search_quantity === 0 ? (
+                        <NoResults />
+                    )
+                        : moviePage ? (
+                            <Container>
+                                <MainTitle>Search results for "{query}" ({searchResults ? search_quantity : ""})</MainTitle>
+                                <Content>
+                                    {search_list_sorted && search_list_sorted.map(movie => (
+                                        <MovieTile
+                                            key={movie.id}
+                                            id={movie.id}
+                                            image={movie.poster_path}
+                                            title={movie.title}
+                                            year={movie.release_date}
+                                            genres={
+                                                movie.genre_ids.map((number) =>
+                                                    genre_list.find((item) =>
+                                                        item.id === number).name
+                                                )
+                                            }
+                                            rating={movie.vote_average}
+                                            votes={movie.vote_count}
+                                        />
+                                    ))}
+                                </Content>
+                                <Pagination />
+                            </Container>
+                        )
+                            : (
+                                <Container>
+                                    <MainTitle>Search results for "{query}" ({searchResults ? search_quantity : ""})</MainTitle>
+                                    <Grid>
+                                        {search_list && search_list.map((person) => (
+                                            <PersonTile
+                                                key={person.id}
+                                                id={person.id}
+                                                image={person.profile_path}
+                                                title={person.original_name}
+                                            />
+                                        ))}
+                                    </Grid>
+                                    <Pagination />
+                                </Container>
+                            )
+            }
+        </>
+    )
+};
+
+export default SearchResults;
---
+++ src/features/SearchResults/styled.js
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const MainTitle = styled.h1`
+    font-weight: 600;
+    font-size: 36px;
+    margin-bottom: 24px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.phonePortrait}) {
+        font-size: 18px;
+        margin-bottom: 12px;
+    }
+`;
+
+export const Content = styled.span`
+    width: 1368px;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 324px);
+    justify-content: center;
+    grid-gap: 24px;
+    margin-bottom: 40px;
+    margin: auto;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.notebook}) {
+        grid-template-columns: repeat(3, auto);
+        width: auto;
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.tabletTwo}) {
+        grid-template-columns: repeat(2, auto);
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.phoneTwo}) {
+        grid-template-columns: repeat(1, 1fr);
+        grid-gap: 16px;
+    }
+`;
+
+export const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 208px);
+    grid-gap: 24px;
+    padding-top: 24px;
+    justify-content: center;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.phone}) {
+        grid-template-columns: repeat(auto-fill, 136px);
+        grid-gap: 16px;
+        padding-top: 16px;
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.phonePortrait}) {
+        padding-top: 12px;
+    }
+`;
