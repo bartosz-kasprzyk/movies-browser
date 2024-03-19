3aa6626ebed5548e441de2e9ae54ef5cd8825b4f
Przemysław Pacek <ppacek@op.pl>
2024-03-19 15:14:43 +0100

    test2

--- src/features/SearchResults/useSearchResults.js
+++ src/features/SearchResults/useSearchResults.js
@@ -2,23 +2,26 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { url_back, url_front_search } from "../../common/API/requests";
 import { useQueryParameter } from "../../common/Header/SearchBar/queryParameters";
-import { useLocation } from "react-router-dom/cjs/react-router-dom.min"; 
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+
 
 export const useSearchResults = () => {
     const query = useQueryParameter("query");
+
     const location = useLocation();
     const isMoviesPage = location.pathname.startsWith("/movies");
     const currentPage = new URLSearchParams(location.search).get("page") || 1;
 
-    const [firstSearch, setFirstSearch] = useState(true); // Dodajemy stan, który śledzi, czy jest to pierwsze uruchomienie wyszukiwania
-
     const [searchResults, setSearchResults] = useState({
-        status: "loading",
+        status: "",
         data: [],
     });
 
     useEffect(() => {
-        setSearchResults({ status: "loading", data: [] });
+        setSearchResults({
+            status: "loading",
+            data: [],
+        });
 
         const debounce = setTimeout(async () => {
             if (query) {
@@ -30,25 +33,17 @@ export const useSearchResults = () => {
                         status: "success",
                         data: response.data
                     });
-
-                    // Przekierowanie na pierwszą stronę tylko przy pierwszym uruchomieniu wyszukiwania
-                    if (firstSearch && currentPage !== 1) {
-                        setFirstSearch(false); // Ustawiamy firstSearch na false, aby zapobiec przekierowaniu przy kolejnych wyszukiwaniach
-                        const newUrl = `${location.pathname}?query=${query}&page=1`;
-                        window.history.replaceState({}, '', newUrl); // Aktualizujemy URL bez przeładowania strony
-                    }
                 } catch (error) {
                     setSearchResults({
                         status: "error",
-                        error: error.message,
                     });
-                    console.error(error);
+                    console.error(error.message);
                 }
             }
         }, 300);
 
         return () => clearTimeout(debounce);
-    }, [query, isMoviesPage, currentPage, firstSearch, location.pathname]);
+    }, [query, isMoviesPage, currentPage]);
 
     return { searchResults };
 };
