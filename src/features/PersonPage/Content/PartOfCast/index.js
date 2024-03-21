1b49fa085163c6be503bec0d0e0e65c5b39247c8
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-03-21 13:56:47 +0100

    disabled cast/crew/part of cast/part of crew when there's no data

--- src/features/PersonPage/Content/PartOfCast/index.js
+++ src/features/PersonPage/Content/PartOfCast/index.js
@@ -15,29 +15,31 @@ export const PartOfCast = () => {
                 null
             )
                 : (
-                    <>
-                        <Title>Movies - Cast ({person_cast ? person_cast.length : ""})</Title>
-                        <Grid>
-                            {person_cast && person_cast.map((movie) => (
-                                <MovieTile
-                                    key={movie.credit_id}
-                                    id={movie.id}
-                                    image={movie.poster_path}
-                                    title={movie.title}
-                                    role={movie.character}
-                                    year={movie.release_date}
-                                    genres={
-                                        movie.genre_ids.map((number) =>
-                                            genre_list?.find((item) =>
-                                                item.id === number).name
-                                        )
-                                    }
-                                    rating={movie.vote_average}
-                                    votes={movie.vote_count}
-                                />
-                            ))}
-                        </Grid>
-                    </>
+                    person_cast && person_cast.length !== 0
+                        ? <>
+                            <Title>Movies - Cast ({person_cast.length})</Title>
+                            <Grid>
+                                {person_cast && person_cast.map((movie) => (
+                                    <MovieTile
+                                        key={movie.credit_id}
+                                        id={movie.id}
+                                        image={movie.poster_path}
+                                        title={movie.title}
+                                        role={movie.character}
+                                        year={movie.release_date}
+                                        genres={
+                                            movie.genre_ids.map((number) =>
+                                                genre_list?.find((item) =>
+                                                    item.id === number).name
+                                            )
+                                        }
+                                        rating={movie.vote_average}
+                                        votes={movie.vote_count}
+                                    />
+                                ))}
+                            </Grid>
+                        </>
+                        : null
                 )}
         </>
     )
--- src/features/PersonPage/Content/PartOfCrew/index.js
+++ src/features/PersonPage/Content/PartOfCrew/index.js
@@ -15,29 +15,31 @@ export const PartOfCrew = () => {
                 null
             )
                 : (
-                    <>
-                        <Title>Movies - Crew ({person_crew ? person_crew.length : ""})</Title>
-                        <Grid>
-                            {person_crew && person_crew.map((movie) => (
-                                <MovieTile
-                                    key={movie.credit_id}
-                                    id={movie.id}
-                                    image={movie.poster_path}
-                                    title={movie.title}
-                                    role={movie.job}
-                                    year={movie.release_date}
-                                    genres={
-                                        movie.genre_ids.map((number) =>
-                                            genre_list.find((item) =>
-                                                item.id === number).name
-                                        )
-                                    }
-                                    rating={movie.vote_average}
-                                    votes={movie.vote_count}
-                                />
-                            ))}
-                        </Grid>
-                    </>
+                    person_crew && person_crew.length !== 0
+                        ? <>
+                            <Title>Movies - Crew ({person_crew.length})</Title>
+                            <Grid>
+                                {person_crew && person_crew.map((movie) => (
+                                    <MovieTile
+                                        key={movie.credit_id}
+                                        id={movie.id}
+                                        image={movie.poster_path}
+                                        title={movie.title}
+                                        role={movie.job}
+                                        year={movie.release_date}
+                                        genres={
+                                            movie.genre_ids.map((number) =>
+                                                genre_list.find((item) =>
+                                                    item.id === number).name
+                                            )
+                                        }
+                                        rating={movie.vote_average}
+                                        votes={movie.vote_count}
+                                    />
+                                ))}
+                            </Grid>
+                        </>
+                        : null
                 )}
         </>
     )
