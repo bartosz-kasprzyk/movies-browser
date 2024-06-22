5d82de851e1b06a56eb77ac744b59e0615e303e6
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-06-22 23:44:11 +0200

    simplified conditional rendering in MovieList

--- src/features/MovieList/index.js
+++ src/features/MovieList/index.js
@@ -13,41 +13,38 @@ const MovieList = () => {
     const { genres } = useGenres();
     const genre_list = genres.data;
 
+    if (popularMovies.status === "loading") {
+        return <Loading />;
+    }
+
+    if (popularMovies.status === "error") {
+        return <Error />;
+    }
+
     return (
-        <>
-            {popularMovies.status === "loading" ? (
-                <Loading />
-            )
-                : popularMovies.status === "error" ? (
-                    <Error />
-                )
-                    : (
-                        <Container>
-                            <Content>
-                                <MainTitle>Popular movies</MainTitle>
-                                {movie_list && movie_list.map(movie => (
-                                    <MovieTile
-                                        key={movie.id}
-                                        id={movie.id}
-                                        image_path={movie.poster_path}
-                                        title={movie.title}
-                                        year={movie.release_date}
-                                        genres={
-                                            movie.genre_ids?.map((number) =>
-                                                genre_list.find((item) =>
-                                                    item.id === number).name
-                                            )
-                                        }
-                                        rating={movie.vote_average}
-                                        votes={movie.vote_count}
-                                    />
-                                ))}
-                            </Content>
-                            <Pagination />
-                        </Container>
-                    )
-            }
-        </>
+        <Container>
+            <Content>
+                <MainTitle>Popular movies</MainTitle>
+                {movie_list && movie_list.map(movie => (
+                    <MovieTile
+                        key={movie.id}
+                        id={movie.id}
+                        image_path={movie.poster_path}
+                        title={movie.title}
+                        year={movie.release_date}
+                        genres={
+                            movie.genre_ids?.map((number) =>
+                                genre_list.find((item) =>
+                                    item.id === number).name
+                            )
+                        }
+                        rating={movie.vote_average}
+                        votes={movie.vote_count}
+                    />
+                ))}
+            </Content>
+            <Pagination />
+        </Container>
     )
 };
 
