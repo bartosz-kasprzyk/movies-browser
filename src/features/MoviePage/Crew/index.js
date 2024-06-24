a9eb54e4d9100162dd3e0bcd4f6fb7945c7aa374
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-06-24 20:49:53 +0200

    simplified conditional rendering in Crew

--- src/features/MoviePage/Crew/index.js
+++ src/features/MoviePage/Crew/index.js
@@ -6,23 +6,23 @@ const Crew = () => {
     const { movieCredits } = useMovieCredits();
     const movie_crew = movieCredits.data.crew;
 
+    if (!movie_crew || movie_crew.length === 0) {
+        return null;
+    }
+
     return (
-        movie_crew && movie_crew.length !== 0
-            ? <>
-                <Grid>
-                    <Title>Crew</Title>
-                    {movie_crew && movie_crew.map((person) => (
-                        <PersonTile
-                            key={person.credit_id}
-                            id={person.id}
-                            image_path={person.profile_path}
-                            title={person.name}
-                            subtitle={person.job}
-                        />
-                    ))}
-                </Grid>
-            </>
-            : null
+        <Grid>
+            <Title>Crew</Title>
+            {movie_crew && movie_crew.map((person) => (
+                <PersonTile
+                    key={person.credit_id}
+                    id={person.id}
+                    image_path={person.profile_path}
+                    title={person.name}
+                    subtitle={person.job}
+                />
+            ))}
+        </Grid>
     )
 };
 
