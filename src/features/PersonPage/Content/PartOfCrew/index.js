6303b55425023232319c23aa38777d610ff24f7d
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-06-24 20:27:23 +0200

    simplified conditional rendering in PartOfCrew

--- src/features/PersonPage/Content/PartOfCrew/index.js
+++ src/features/PersonPage/Content/PartOfCrew/index.js
@@ -9,38 +9,35 @@ export const PartOfCrew = () => {
     const { genres } = useGenres();
     const genre_list = genres.data;
 
+    if (genres.status === "loading") {
+        return null;
+    }
+
+    if (!person_crew || person_crew.length === 0) {
+        return null;
+    }
+
     return (
-        <>
-            {genres.status === "loading" ? (
-                null
-            )
-                : (
-                    person_crew && person_crew.length !== 0
-                        ? <>
-                            <Grid>
-                                <Title>Movies - Crew ({person_crew.length})</Title>
-                                {person_crew && person_crew.map((movie) => (
-                                    <MovieTile
-                                        key={movie.credit_id}
-                                        id={movie.id}
-                                        image_path={movie.poster_path}
-                                        title={movie.title}
-                                        role={movie.job}
-                                        year={movie.release_date}
-                                        genres={
-                                            movie.genre_ids?.map((number) =>
-                                                genre_list?.find((item) =>
-                                                    item.id === number).name
-                                            )
-                                        }
-                                        rating={movie.vote_average}
-                                        votes={movie.vote_count}
-                                    />
-                                ))}
-                            </Grid>
-                        </>
-                        : null
-                )}
-        </>
+        <Grid>
+            <Title>Movies - Crew ({person_crew.length})</Title>
+            {person_crew && person_crew.map((movie) => (
+                <MovieTile
+                    key={movie.credit_id}
+                    id={movie.id}
+                    image_path={movie.poster_path}
+                    title={movie.title}
+                    role={movie.job}
+                    year={movie.release_date}
+                    genres={
+                        movie.genre_ids?.map((number) =>
+                            genre_list?.find((item) =>
+                                item.id === number).name
+                        )
+                    }
+                    rating={movie.vote_average}
+                    votes={movie.vote_count}
+                />
+            ))}
+        </Grid>
     )
 };
