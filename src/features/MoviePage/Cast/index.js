edd58dbab589b72a79164a80f86f8b3269023a0a
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-06-24 20:49:19 +0200

    simplified conditional rendering in Cast

--- src/features/MoviePage/Cast/index.js
+++ src/features/MoviePage/Cast/index.js
@@ -6,23 +6,23 @@ const Cast = () => {
     const { movieCredits } = useMovieCredits();
     const movie_cast = movieCredits.data.cast;
 
+    if (!movie_cast || movie_cast.length === 0) {
+        return null;
+    }
+
     return (
-        movie_cast && movie_cast.length !== 0
-            ? <>
-                <Grid>
-                    <Title>Cast</Title>
-                    {movie_cast && movie_cast.map((person) => (
-                        <PersonTile
-                            key={person.credit_id}
-                            id={person.id}
-                            image_path={person.profile_path}
-                            title={person.name}
-                            subtitle={person.character}
-                        />
-                    ))}
-                </Grid>
-            </>
-            : null
+        <Grid>
+            <Title>Cast</Title>
+            {movie_cast && movie_cast.map((person) => (
+                <PersonTile
+                    key={person.credit_id}
+                    id={person.id}
+                    image_path={person.profile_path}
+                    title={person.name}
+                    subtitle={person.character}
+                />
+            ))}
+        </Grid>
     )
 };
 
