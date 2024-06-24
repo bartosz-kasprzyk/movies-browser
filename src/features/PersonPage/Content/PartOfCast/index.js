94cab60142db821c718dca7bf3b054723071bf5a
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-06-24 20:26:15 +0200

    simplified conditional rendering in PartOfCast

--- src/features/PersonPage/Content/PartOfCast/index.js
+++ src/features/PersonPage/Content/PartOfCast/index.js
@@ -9,38 +9,35 @@ export const PartOfCast = () => {
     const { genres } = useGenres();
     const genre_list = genres.data;
 
+    if (genres.status === "loading") {
+        return null;
+    }
+
+    if (!person_cast || person_cast.length === 0) {
+        return null;
+    }
+
     return (
-        <>
-            {genres.status === "loading" ? (
-                null
-            )
-                : (
-                    person_cast && person_cast.length !== 0
-                        ? <>
-                            <Grid>
-                                <Title>Movies - Cast ({person_cast.length})</Title>
-                                {person_cast && person_cast.map((movie) => (
-                                    <MovieTile
-                                        key={movie.credit_id}
-                                        id={movie.id}
-                                        image_path={movie.poster_path}
-                                        title={movie.title}
-                                        role={movie.character}
-                                        year={movie.release_date}
-                                        genres={
-                                            movie.genre_ids?.map((number) =>
-                                                genre_list?.find((item) =>
-                                                    item.id === number).name
-                                            )
-                                        }
-                                        rating={movie.vote_average}
-                                        votes={movie.vote_count}
-                                    />
-                                ))}
-                            </Grid>
-                        </>
-                        : null
-                )}
-        </>
+        <Grid>
+            <Title>Movies - Cast ({person_cast.length})</Title>
+            {person_cast && person_cast.map((movie) => (
+                <MovieTile
+                    key={movie.credit_id}
+                    id={movie.id}
+                    image_path={movie.poster_path}
+                    title={movie.title}
+                    role={movie.character}
+                    year={movie.release_date}
+                    genres={
+                        movie.genre_ids?.map((number) =>
+                            genre_list?.find((item) =>
+                                item.id === number).name
+                        )
+                    }
+                    rating={movie.vote_average}
+                    votes={movie.vote_count}
+                />
+            ))}
+        </Grid>
     )
 };
